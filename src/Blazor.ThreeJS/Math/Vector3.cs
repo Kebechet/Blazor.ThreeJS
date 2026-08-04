a1573263ae82f9b0ff3e8fb191ba4fa15385cb8d
Kebechet <samosidor@gmail.com>
2026-08-04 16:03:04 +0200

    docs: add comprehensive XML documentation to Vector3 public members

--- src/Blazor.ThreeJS/Math/Vector3.cs
+++ src/Blazor.ThreeJS/Math/Vector3.cs
@@ -16,10 +16,19 @@ public sealed class Vector3
 	/// </summary>
 	internal Action? OnChange { get; set; }
 
+	/// <summary>
+	/// Initializes a new vector with all components set to zero.
+	/// </summary>
 	public Vector3()
 	{
 	}
 
+	/// <summary>
+	/// Initializes a new vector with the given component values.
+	/// </summary>
+	/// <param name="x">The X component.</param>
+	/// <param name="y">The Y component.</param>
+	/// <param name="z">The Z component.</param>
 	public Vector3(float x, float y, float z)
 	{
 		_x = x;
@@ -27,6 +36,9 @@ public sealed class Vector3
 		_z = z;
 	}
 
+	/// <summary>
+	/// Gets or sets the X component. Setting this component triggers the <c>OnChange</c> callback.
+	/// </summary>
 	public float X
 	{
 		get { return _x; }
@@ -37,6 +49,9 @@ public sealed class Vector3
 		}
 	}
 
+	/// <summary>
+	/// Gets or sets the Y component. Setting this component triggers the <c>OnChange</c> callback.
+	/// </summary>
 	public float Y
 	{
 		get { return _y; }
@@ -47,6 +62,9 @@ public sealed class Vector3
 		}
 	}
 
+	/// <summary>
+	/// Gets or sets the Z component. Setting this component triggers the <c>OnChange</c> callback.
+	/// </summary>
 	public float Z
 	{
 		get { return _z; }
@@ -57,6 +75,13 @@ public sealed class Vector3
 		}
 	}
 
+	/// <summary>
+	/// Sets all three components and triggers the <c>OnChange</c> callback once (not per component).
+	/// </summary>
+	/// <param name="x">The new X component.</param>
+	/// <param name="y">The new Y component.</param>
+	/// <param name="z">The new Z component.</param>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 Set(float x, float y, float z)
 	{
 		_x = x;
@@ -66,36 +91,70 @@ public sealed class Vector3
 		return this;
 	}
 
+	/// <summary>
+	/// Copies the components from another vector into this vector, mutating it in place.
+	/// </summary>
+	/// <param name="other">The vector to copy from.</param>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 Copy(Vector3 other)
 	{
 		return Set(other._x, other._y, other._z);
 	}
 
+	/// <summary>
+	/// Adds another vector's components to this vector's components, mutating this vector in place.
+	/// </summary>
+	/// <param name="other">The vector to add.</param>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 Add(Vector3 other)
 	{
 		return Set(_x + other._x, _y + other._y, _z + other._z);
 	}
 
+	/// <summary>
+	/// Subtracts another vector's components from this vector's components, mutating this vector in place.
+	/// </summary>
+	/// <param name="other">The vector to subtract.</param>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 Sub(Vector3 other)
 	{
 		return Set(_x - other._x, _y - other._y, _z - other._z);
 	}
 
+	/// <summary>
+	/// Multiplies each component of this vector by a scalar value, mutating this vector in place.
+	/// </summary>
+	/// <param name="scalar">The scalar to multiply by.</param>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 MultiplyScalar(float scalar)
 	{
 		return Set(_x * scalar, _y * scalar, _z * scalar);
 	}
 
+	/// <summary>
+	/// Computes the squared length (magnitude squared) of this vector. This is useful for comparisons
+	/// to avoid the cost of computing the square root when only relative magnitude matters.
+	/// </summary>
+	/// <returns>The squared length of this vector.</returns>
 	public float LengthSq()
 	{
 		return (_x * _x) + (_y * _y) + (_z * _z);
 	}
 
+	/// <summary>
+	/// Computes the length (magnitude) of this vector: sqrt(x² + y² + z²).
+	/// </summary>
+	/// <returns>The length of this vector.</returns>
 	public float Length()
 	{
 		return MathF.Sqrt(LengthSq());
 	}
 
+	/// <summary>
+	/// Normalizes this vector to unit length (length = 1), mutating it in place.
+	/// If this vector has zero length, it is left unchanged rather than producing NaN.
+	/// </summary>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 Normalize()
 	{
 		var length = Length();
@@ -107,11 +166,21 @@ public sealed class Vector3
 		return MultiplyScalar(1f / length);
 	}
 
+	/// <summary>
+	/// Computes the dot product of this vector with another vector.
+	/// </summary>
+	/// <param name="other">The other vector.</param>
+	/// <returns>The dot product: (this.x * other.x) + (this.y * other.y) + (this.z * other.z).</returns>
 	public float Dot(Vector3 other)
 	{
 		return (_x * other._x) + (_y * other._y) + (_z * other._z);
 	}
 
+	/// <summary>
+	/// Computes the cross product of this vector with another vector, storing the result in this vector, mutating it in place.
+	/// </summary>
+	/// <param name="other">The other vector.</param>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 Cross(Vector3 other)
 	{
 		return Set(
@@ -120,6 +189,11 @@ public sealed class Vector3
 			(_x * other._y) - (_y * other._x));
 	}
 
+	/// <summary>
+	/// Computes the distance from this vector to another vector.
+	/// </summary>
+	/// <param name="other">The other vector.</param>
+	/// <returns>The Euclidean distance between the two points represented by these vectors.</returns>
 	public float DistanceTo(Vector3 other)
 	{
 		var deltaX = _x - other._x;
@@ -128,16 +202,30 @@ public sealed class Vector3
 		return MathF.Sqrt((deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ));
 	}
 
+	/// <summary>
+	/// Extracts the components of this vector into an array.
+	/// </summary>
+	/// <returns>A new array containing [x, y, z].</returns>
 	public float[] ToArray()
 	{
 		return [_x, _y, _z];
 	}
 
+	/// <summary>
+	/// Copies three values from an array into this vector's components, mutating it in place.
+	/// </summary>
+	/// <param name="values">An array with at least three elements. The first three elements are copied.</param>
+	/// <returns>This vector, for method chaining.</returns>
 	public Vector3 FromArray(float[] values)
 	{
 		return Set(values[0], values[1], values[2]);
 	}
 
+	/// <summary>
+	/// Creates a shallow copy of this vector with the same component values.
+	/// The copy is independent and has no <c>OnChange</c> callback set.
+	/// </summary>
+	/// <returns>A new vector with the same components.</returns>
 	public Vector3 Clone()
 	{
 		return new Vector3(_x, _y, _z);
